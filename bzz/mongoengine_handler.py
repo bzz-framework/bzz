59eff444088d7b7eb494ecf0b6229286c219db13
Bernardo Heynemann <heynemann@gmail.com>
2014-07-01 10:33:36 -0300

    Refactoring mongodb code to be in mongoengine handler as well as be async

--- bzz/mongoengine_handler.py
+++ bzz/mongoengine_handler.py
@@ -8,6 +8,8 @@
 # http://www.opensource.org/licenses/MIT-license
 # Copyright (c) 2014 Bernardo Heynemann heynemann@gmail.com
 
+import math
+
 import tornado.gen as gen
 
 import bzz.rest as bzz
@@ -29,10 +31,8 @@ class MongoEngineRestHandler(bzz.ModelRestHandler):
             (details_url, cls, dict(model=document_type, name=name, prefix=prefix))
         ]
 
-    def initialize(self, model, name, prefix):
-        self.model = model
-        self.name = name
-        self.prefix = prefix
+    def get_model_id(self, instance):
+        return getattr(instance, 'get_id', lambda: str(instance.id))()
 
     @gen.coroutine
     def save_new_instance(self, data):
@@ -45,21 +45,71 @@ class MongoEngineRestHandler(bzz.ModelRestHandler):
 
         raise gen.Return(instance)
 
-    def collect_arguments(self):
-        return self.request.arguments
-
-    def get_model_id(self, instance):
-        return getattr(instance, 'get_id', lambda: str(instance.id))()
+    @gen.coroutine
+    def update_instance(self, pk, data):
+        instance = yield self.get_instance(pk)
+        for field, value in self.get_request_data().items():
+            setattr(instance, field, value)
+        instance.save()
 
     @gen.coroutine
-    def post(self, pk=None):
-        result = yield self.save_new_instance(self.collect_arguments())
+    def delete_instance(self, pk):
+        instance = yield self.get_instance(pk)
+        if instance is not None:
+            instance.delete()
+        raise gen.Return(instance)
 
-        self.write('OK')
-        model_id = self.get_model_id(result)
-        self.set_header('X-Created-Id', model_id)
-        self.set_header('location', '/%s%s/%s/' % (
-            self.prefix,
-            self.name,
-            model_id
-        ))
+    @gen.coroutine
+    def get_instance(self, instance_id):
+        instance = None
+        field = self.get_id_field_name()
+
+        if instance_id:
+            instance = self.model.objects.get(**{field: instance_id})
+
+        raise gen.Return(instance)
+
+    @gen.coroutine
+    def get_list(self, per_page=20):
+        pages = int(math.ceil(self.model.objects.count() / float(per_page)))
+        try:
+            page = int(self.get_argument('page', 1))
+        except ValueError:
+            page = 1
+
+        if page > pages:
+            page = pages
+
+        page -= 1
+
+        start = per_page * page
+        stop = start + per_page
+
+        items = self.model.objects.all()[start:stop]
+        raise gen.Return(items)
+
+    def dump_object(self, instance):
+        method = getattr(instance, 'to_dict', None)
+
+        if method:
+            return method()
+
+        data = {}
+        for field in instance._fields.keys():
+            data[field] = str(getattr(instance, field, None))
+        return data
+
+    def get_instance_id(self, instance):
+        field = getattr(instance, 'get_id_field_name', None)
+        if field:
+            return getattr(instance, field().name)
+
+        return str(instance.id)
+
+    def get_id_field_name(self):
+        field = getattr(self.model, 'get_id_field_name', None)
+        if field:
+            return field().name
+
+        return 'id'
+
--- bzz/rest.py
+++ bzz/rest.py
@@ -8,8 +8,8 @@
 # http://www.opensource.org/licenses/MIT-license
 # Copyright (c) 2014 Bernardo Heynemann heynemann@gmail.com
 
-import math
 import tornado
+import tornado.gen as gen
 
 try:
     import ujson as json
@@ -18,64 +18,58 @@ except ImportError:
 
 
 class ModelRestHandler(tornado.web.RequestHandler):
-    def initialize(self, model):
+    def initialize(self, model, name, prefix):
         self.model = model
+        self.name = name
+        self.prefix = prefix
 
+    def write_json(self, obj):
+        self.set_header("Content-Type", "application/json")
+        self.write(json.dumps(obj))
+
+    @gen.coroutine
     def get(self, pk=None):
-        instance = self.get_instance(pk)
+        instance = yield self.get_instance(pk)
 
         if instance:
-            self.write(json.dumps(self.dump_object(instance)))
+            self.write_json(self.dump_object(instance))
+            self.finish()
         else:
-            self.list()
+            yield self.list()
 
+    @gen.coroutine
     def post(self, pk=None):
-        instance = self.model(**self.get_request_data())
-        instance.save()
-        pk = self.get_object_id(instance)
+        instance = yield self.save_new_instance(self.get_request_data())
+        pk = self.get_instance_id(instance)
         self.set_header('X-Created-Id', pk)
+        self.set_header('location', '/%s%s/%s/' % (
+            self.prefix,
+            self.name,
+            pk
+        ))
         self.write('OK')
 
+    @gen.coroutine
     def put(self, pk):
-        instance = self.get_instance(pk)
-
-        for field, value in self.get_request_data().items():
-            setattr(instance, field, value)
-
-        instance.save()
+        yield self.update_instance(pk, self.get_request_data())
         self.write('OK')
 
+    @gen.coroutine
     def delete(self, pk):
-        instance = self.get_instance(pk)
+        instance = yield self.delete_instance(pk)
         if instance:
-            instance.delete()
             self.write('OK')
         else:
             self.write('FAIL')
 
+    @gen.coroutine
     def list(self):
+        items = yield self.get_list()
         dump = []
-        for obj in self.paginate(self.model.objects):
-            dump.append(self.dump_object(obj))
+        for item in items:
+            dump.append(self.dump_object(item))
 
-        self.write(json.dumps(dump))
-
-    def paginate(self, queryset, per_page=20):
-        pages = int(math.ceil(queryset.count() / float(per_page)))
-        try:
-            page = int(self.get_argument('page', 1))
-        except ValueError:
-            page = 1
-
-        if page > pages:
-            page = pages
-
-        page -= 1
-
-        start = per_page * page
-        stop = start + per_page
-
-        return queryset.all()[start:stop]
+        self.write_json(dump)
 
     def get_request_data(self):
         data = {}
@@ -85,36 +79,8 @@ class ModelRestHandler(tornado.web.RequestHandler):
                 data[arg] = None
         return data
 
-    def get_object_id(self, instance):
-        field = getattr(instance, 'get_id_field', None)
-        instance_id = instance.id
-        if field:
-            return getattr(instance, field().name)
-
-        return str(instance_id)
-
-    def get_id_field(self):
-        field = getattr(self.model, 'get_id_field', None)
-        if field:
-            return field().name
-
-        return 'id'
-
     def dump_object(self, instance):
-        method = getattr(instance, 'to_dict', None)
+        return json.dumps(instance)
 
-        if method:
-            return method()
-
-        data = {}
-        for field in instance._fields.keys():
-            data[field] = str(getattr(instance, field, None))
-        return data
-
-    def get_instance(self, instance_id):
-        instance = None
-        field = self.get_id_field()
-
-        if instance_id:
-            instance = self.model.objects.get(**{field: instance_id})
-        return instance
+    def collect_arguments(self):
+        return self.request.arguments
