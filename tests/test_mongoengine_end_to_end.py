7017eaa39216716e0fabb2417630335b60c0c469
Bernardo Heynemann <heynemann@gmail.com>
2014-07-10 18:28:35 -0300

    More end-to-end tests

--- tests/test_mongoengine_end_to_end.py
+++ tests/test_mongoengine_end_to_end.py
@@ -23,6 +23,127 @@ import bzz.utils as utils
 import tests.base as base
 
 
+class MongoEngineEndToEndTestCase(base.ApiTestCase):
+    def __get_test_data(self):
+        return [
+            ('GET', '/user', dict(), 200, lambda body: load_json(body), []),
+            ('POST', '/user', dict(body="name=test%20user&age=32"), 200, None, 'OK'),
+            ('GET', '/user', dict(), 200, lambda body: load_json(body), self.__assert_user_data(name="test user", age=32)),
+            ('GET', '/user/test%20user', dict(), 200, lambda body: load_json(body), self.__assert_user_data(name="test user", age=32)),
+            ('PUT', '/user/test%20user', dict(body="age=31"), 200, None, 'OK'),
+            ('GET', '/user/test%20user', dict(), 200, lambda body: load_json(body), self.__assert_user_data(name="test user", age=31)),
+            ('POST', '/user', dict(body="name=test-user2&age=32"), 200, None, 'OK'),
+            ('DELETE', '/user/test-user2', dict(), 200, None, 'OK'),
+            ('GET', '/user', dict(), 200, lambda body: load_json(body), self.__assert_len(1)),
+            ('GET', '/team', dict(), 200, lambda body: load_json(body), []),
+            ('POST', '/team', dict(body="name=team-1&owner=test%20user"), 200, None, 'OK'),
+            ('GET', '/team/team-1', dict(), 200, lambda body: load_json(body), self.__assert_team_data(name="team-1", owner="test user")),
+            ('POST', '/user', dict(body="name=test-user3&age=32"), 200, None, 'OK'),
+            ('PUT', '/team/team-1', dict(body="owner=test-user3"), 200, None, 'OK'),
+            ('POST', '/team', dict(body="name=team-2&owner=test%20user"), 200, None, 'OK'),
+            ('DELETE', '/team/team-2', dict(), 200, None, 'OK'),
+        ]
+
+    def setUp(self):
+        super(MongoEngineEndToEndTestCase, self).setUp()
+        signals.post_create_instance.receivers = {}
+        signals.post_update_instance.receivers = {}
+        signals.post_delete_instance.receivers = {}
+        User.objects.delete()
+        Team.objects.delete()
+
+    def get_config(self):
+        return dict(
+            MONGO_DATABASES={
+                'default': {
+                    'host': 'localhost',
+                    'port': 3334,
+                    'database': 'bzz_test'
+                }
+            },
+        )
+
+    def get_server(self):
+        cfg = config.Config(**self.get_config())
+        self.server = TestServer(config=cfg)
+        return self.server
+
+    def __assert_user_data(self, created_at=None, age=None, id_=None, name=None):
+        def handle(obj):
+            if isinstance(obj, (list, tuple)):
+                obj = obj[0]
+
+            if created_at is not None:
+                expect(obj['created_at']).to_be_like(created_at)
+
+            if age is not None:
+                expect(obj['age']).to_equal(age)
+
+            if id_ is not None:
+                expect(obj['id']).to_be_like(id_)
+
+            if name is not None:
+                expect(obj['name']).to_equal(name)
+
+        return handle
+
+    def __assert_team_data(self, owner=None, name=None):
+        def handle(obj):
+            if isinstance(obj, (list, tuple)):
+                obj = obj[0]
+
+            if owner is not None:
+                expect(obj['owner']).not_to_be_null()
+                expect(obj['owner'].name).to_equal(owner)
+
+            if name is not None:
+                expect(obj['name']).to_equal(name)
+
+        return handle
+
+
+    def __assert_len(self, expected_length):
+        def handle(obj):
+            expect(obj).not_to_be_null()
+            expect(obj).to_be_instance_of(list)
+            expect(obj).to_length(expected_length)
+        return handle
+
+    def validate_request(self, url_arguments):
+        method, url, options, expected_status_code, transform_body, expected_body = url_arguments
+
+        self.http_client.fetch(
+            self.get_url(url),
+            method=method,
+            callback=self.stop,
+            **options
+        )
+        response = self.wait()
+        expect(response.code).to_equal(expected_status_code)
+        print("%s %s - %s" % (method, url, response.code))
+
+        body = response.body
+        if transform_body is not None:
+            body = transform_body(response.body)
+
+        if isinstance(expected_body, types.FunctionType):
+            expected_body(body)
+        else:
+            expect(body).to_be_like(expected_body)
+
+    def test_end_to_end_flow(self):
+        data = self.__get_test_data()
+
+        print("")
+        print("")
+        print("")
+        print("Doing end-to-end test:")
+        print("")
+        for url_arguments in data:
+            self.validate_request(url_arguments)
+        print("")
+
+
 def load_json(json_string):
     try:
         return utils.loads(json_string)
@@ -87,100 +208,4 @@ class TestServer(server.Server):
         return [route for route_list in routes for route in route_list]
 
 
-class MongoEngineEndToEndTestCase(base.ApiTestCase):
-    def setUp(self):
-        super(MongoEngineEndToEndTestCase, self).setUp()
-        signals.post_create_instance.receivers = {}
-        signals.post_update_instance.receivers = {}
-        signals.post_delete_instance.receivers = {}
-        User.objects.delete()
-        Team.objects.delete()
 
-    def get_config(self):
-        return dict(
-            MONGO_DATABASES={
-                'default': {
-                    'host': 'localhost',
-                    'port': 3334,
-                    'database': 'bzz_test'
-                }
-            },
-        )
-
-    def get_server(self):
-        cfg = config.Config(**self.get_config())
-        self.server = TestServer(config=cfg)
-        return self.server
-
-    def __assert_user_data(self, created_at=None, age=None, id_=None, name=None):
-        def handle(obj):
-            if isinstance(obj, (list, tuple)):
-                obj = obj[0]
-
-            if created_at is not None:
-                expect(obj['created_at']).to_be_like(created_at)
-
-            if age is not None:
-                expect(obj['age']).to_equal(age)
-
-            if id_ is not None:
-                expect(obj['id']).to_be_like(id_)
-
-            if name is not None:
-                expect(obj['name']).to_equal(name)
-
-        return handle
-
-    def __assert_len(self, expected_length):
-        def handle(obj):
-            expect(obj).not_to_be_null()
-            expect(obj).to_be_instance_of(list)
-            expect(obj).to_length(expected_length)
-        return handle
-
-    def __get_test_data(self):
-        return [
-            ('GET', '/user', dict(), 200, lambda body: load_json(body), []),
-            ('POST', '/user', dict(body="name=test%20user&age=32"), 200, None, 'OK'),
-            ('GET', '/user', dict(), 200, lambda body: load_json(body), self.__assert_user_data(name="test user", age=32)),
-            ('GET', '/user/test%20user', dict(), 200, lambda body: load_json(body), self.__assert_user_data(name="test user", age=32)),
-            ('PUT', '/user/test%20user', dict(body="age=31"), 200, None, 'OK'),
-            ('GET', '/user/test%20user', dict(), 200, lambda body: load_json(body), self.__assert_user_data(name="test user", age=31)),
-            ('POST', '/user', dict(body="name=test-user2&age=32"), 200, None, 'OK'),
-            ('DELETE', '/user/test-user2', dict(), 200, None, 'OK'),
-            ('GET', '/user', dict(), 200, lambda body: load_json(body), self.__assert_len(1)),
-        ]
-
-    def test_end_to_end_flow(self):
-        data = self.__get_test_data()
-
-        print("")
-        print("")
-        print("")
-        print("Doing end-to-end test:")
-        print("")
-        for url_arguments in data:
-            self.validate_request(url_arguments)
-        print("")
-
-    def validate_request(self, url_arguments):
-        method, url, options, expected_status_code, transform_body, expected_body = url_arguments
-
-        self.http_client.fetch(
-            self.get_url(url),
-            method=method,
-            callback=self.stop,
-            **options
-        )
-        response = self.wait()
-        expect(response.code).to_equal(expected_status_code)
-        print("%s %s - %s" % (method, url, response.code))
-
-        body = response.body
-        if transform_body is not None:
-            body = transform_body(response.body)
-
-        if isinstance(expected_body, types.FunctionType):
-            expected_body(body)
-        else:
-            expect(body).to_be_like(expected_body)
