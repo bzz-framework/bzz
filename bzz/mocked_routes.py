861605e18b0782dda204eab11b81f7e05dc0f3fd
Guilherme Souza <guivideojob@gmail.com>
2014-07-11 15:33:03 -0300

    MockedRoutes

---
+++ bzz/mocked_routes.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This file is part of bzz.
+# https://github.com/heynemann/bzz
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2014 Bernardo Heynemann heynemann@gmail.com
+
+import tornado.web
+
+
+class MockedRoutesHandler(tornado.web.RequestHandler):
+    def __init__(self, *args, **kwargs):
+        self.handler_methods = kwargs.pop('handler_methods')
+        super(MockedRoutesHandler, self).__init__(*args, **kwargs)
+
+
+    def prepare(self):
+        if self.request.method in self.handler_methods or '*' in self.handler_methods:
+            response = self.handler_methods.get(self.request.method, None) or self.handler_methods.get('*')
+            status = response.get('status', 200)
+            body = response.get('body', '')
+
+            if status >= 400:
+                raise tornado.web.HTTPError(status, body)
+
+            if 'cookies' in response:
+                for cookie, value in response['cookies'].iteritems():
+                    self.set_cookie(cookie, value)
+
+            self.write(body)
+            self.set_status(status)
+            self.finish()
+
+class MockedRoutes():
+
+    def __init__(self, routes_tuple):
+        self.routes_tuple = routes_tuple
+        self.routes = {}
+        self.tornado_routes = []
+
+    def handlers(self):
+        for route in self.routes_tuple:
+            if not route[1] in self.routes:
+                self.routes[route[1]] = {}
+            if not route[0] in self.routes[route[1]]:
+                self.routes[route[1]][route[0]] = {}
+            if len(route) > 2:
+                result = route[2]
+            else:
+                result = {'body': '', 'status': 200}
+
+            self.routes[route[1]][route[0]] = result
+
+        for route, methods in self.routes.iteritems():
+            self.tornado_routes.append((route, MockedRoutesHandler, dict(handler_methods=methods)))
+        return self.tornado_routes
+
---
+++ tests/mocked_routes/test_mocked_routes.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This file is part of bzz.
+# https://github.com/heynemann/bzz
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2014 Bernardo Heynemann heynemann@gmail.com
+
+import tornado.web
+import cow.server as server
+from preggy import expect
+import tornado.testing as testing
+import derpconf.config as config
+from nose_focus import focus
+from tornado.httpclient import HTTPError
+
+import tests.base as base
+import bzz.mocked_routes as mocked_routes
+
+
+class TestServer(server.Server):
+
+    def get_handlers(self):
+        routes = mocked_routes.MockedRoutes([
+            ('GET', '/much/api', dict(body='much api')),
+            ('POST', '/much/api'),
+            ('*', '/much/match', dict(body='such match')),
+            ('*', r'/such/.*', dict(body='such match')),
+            ('GET', '/much/error', dict(body='WOW', status=404)),
+            ('GET', '/much/authentication', dict(body='WOW', cookies={'super': 'cow'})),
+        ])
+        return routes.handlers()
+
+
+class TestMockedRoutes(base.ApiTestCase):
+    def get_config(self):
+        return {}
+
+    def get_server(self):
+        cfg = config.Config(**self.get_config())
+        self.server = TestServer(config=cfg)
+        return self.server
+
+    @testing.gen_test
+    def test_can_get_exactly_match(self):
+        response = yield self.http_client.fetch(
+            self.get_url('/much/api'),
+        )
+        expect(response.code).to_equal(200)
+        expect(response.body).to_equal('much api')
+
+    @testing.gen_test
+    def test_can_post_exactly_match(self):
+        response = yield self.http_client.fetch(
+            self.get_url('/much/api'),
+            method='POST',
+            body='much anything'
+        )
+        expect(response.code).to_equal(200)
+        expect(response.body).to_equal('')
+
+    @testing.gen_test
+    def test_can_get_method_wildcard(self):
+        response = yield self.http_client.fetch(
+            self.get_url('/much/match'),
+        )
+        expect(response.code).to_equal(200)
+        expect(response.body).to_equal('such match')
+
+    @testing.gen_test
+    def test_can_post_method_wildcard(self):
+        response = yield self.http_client.fetch(
+            self.get_url('/much/match'),
+            method='POST',
+            body='much wow'
+        )
+        expect(response.code).to_equal(200)
+        expect(response.body).to_equal('such match')
+
+    @testing.gen_test
+    def test_can_get_url_regex(self):
+        response = yield self.http_client.fetch(
+            self.get_url('/such/match'),
+        )
+        expect(response.code).to_equal(200)
+        expect(response.body).to_equal('such match')
+
+    @testing.gen_test
+    def test_can_post_url_regex(self):
+        response = yield self.http_client.fetch(
+            self.get_url('/such/match'),
+            method='POST',
+            body='such magic'
+        )
+        expect(response.code).to_equal(200)
+        expect(response.body).to_equal('such match')
+
+    @testing.gen_test
+    def test_can_get_cookies(self):
+        response = yield self.http_client.fetch(
+            self.get_url('/much/authentication')
+        )
+        expect(response.code).to_equal(200)
+        expect(response.body).to_equal('WOW')
+        expect(response.headers['Set-Cookie']).to_equal('super=cow; Path=/')
+
+    @testing.gen_test
+    def test_can_set_error(self):
+        err = expect.error_to_happen(HTTPError)
+        with err:
+            response = yield self.http_client.fetch(
+                self.get_url('/much/error'),
+                method='GET'
+            )
+        expect(err).to_have_an_error_message_of('HTTP 404: Not Found')
+
+    @testing.gen_test
+    def test_cant_delete_exactly_match(self):
+        err = expect.error_to_happen(HTTPError)
+        with err:
+            response = yield self.http_client.fetch(
+                self.get_url('/much/api'),
+                method='DELETE'
+            )
+        expect(err).to_have_an_error_message_of('HTTP 405: Method Not Allowed')
