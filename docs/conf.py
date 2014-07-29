145c3b46ab9ec673d9cd0619a34f6cf7cbd44b63
Bernardo Heynemann <heynemann@gmail.com>
2014-07-29 13:21:45 -0300

    Conf for autosource

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import sys
+import os
+
 #
 # bzz documentation build configuration file, created by
 # sphinx-quickstart on Tue Jul  1 11:11:08 2014.
@@ -15,7 +18,7 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
